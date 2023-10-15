8a813b8c66a8e6520b917bd40d0145d15c489511
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-10-15 19:25:13 -0300

    Add example to transform a discriminated union into a union

--- src/learning/typescript-core/mapped-types.ts
+++ src/learning/typescript-core/mapped-types.ts
@@ -225,3 +225,32 @@ import { Equal, Expect } from '../../test-utils';
     >,
   ];
 }
+
+/**
+ * Tranforming a discriminated union into a union
+ */
+{
+  type Fruit =
+    | {
+        name: 'apple';
+        color: 'red';
+      }
+    | {
+        name: 'banana';
+        color: 'yellow';
+      }
+    | {
+        name: 'orange';
+        color: 'orange';
+      };
+
+  type TransformedFruit = {
+    [K in Fruit as K['name']]: `${K['name']}:${K['color']}`;
+  }[Fruit['name']];
+
+  type Tests = [
+    Expect<
+      Equal<TransformedFruit, 'apple:red' | 'banana:yellow' | 'orange:orange'>
+    >,
+  ];
+}
