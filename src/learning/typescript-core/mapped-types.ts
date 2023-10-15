ec464e0d0702e42cb3589b67ac3905cdd89ad378
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-10-15 19:19:31 -0300

    Add example to transform an object into a union of template literals

--- src/learning/typescript-core/mapped-types.ts
+++ src/learning/typescript-core/mapped-types.ts
@@ -204,3 +204,24 @@ import { Equal, Expect } from '../../test-utils';
     >,
   ];
 }
+
+/**
+ * Transforming an object into a union of template literals
+ */
+{
+  interface FruitMap {
+    apple: 'red';
+    banana: 'yellow';
+    orange: 'orange';
+  }
+
+  type TransformedFruit = {
+    [K in keyof FruitMap]: `${K}:${FruitMap[K]}`;
+  }[keyof FruitMap];
+
+  type Tests = [
+    Expect<
+      Equal<TransformedFruit, 'apple:red' | 'banana:yellow' | 'orange:orange'>
+    >,
+  ];
+}
