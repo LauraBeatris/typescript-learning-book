54c401d9a8b63caf2a8c401f556dc192debf4cf0
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-10-20 15:57:14 -0300

    Add example to restrict generic argument types

--- src/learning/generics/basics.ts
+++ src/learning/generics/basics.ts
@@ -11,8 +11,30 @@ import { Equal, Expect } from '../../test-utils';
   const one = returnWhatIPassIn(1);
   const matt = returnWhatIPassIn('matt');
 
-  type tests = [
+  type Tests = [
     Expect<Equal<typeof one, 1>>,
     Expect<Equal<typeof matt, 'matt'>>,
   ];
+}
+
+/**
+ * Restricting generic argument types
+ */
+{
+  const returnWhatIPassIn = <T extends string>(t: T) => t;
+
+  const a = returnWhatIPassIn('a');
+
+  type Tests = [Expect<Equal<typeof a, 'a'>>];
+
+  // @ts-expect-error
+  returnWhatIPassIn(1);
+
+  // @ts-expect-error
+  returnWhatIPassIn(true);
+
+  // @ts-expect-error
+  returnWhatIPassIn({
+    foo: 'bar',
+  });
 }
