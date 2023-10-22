7209aeb0350090df6918b45fb7d906a39be37f1b
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-10-22 18:17:12 -0300

    Add basics of type arguments

---
+++ src/learning/generics/type-arguments-basics.ts
@@ -0,0 +1,68 @@
+import { Equal, Expect } from '../../test-utils';
+
+/**
+ * Adding type parameters to a function
+ */
+{
+  const createSet = <T>() => {
+    return new Set<T>();
+  };
+
+  const stringSet = createSet<string>();
+  const numberSet = createSet<number>();
+  const unknownSet = createSet();
+
+  type Tests = [
+    Expect<Equal<typeof stringSet, Set<string>>>,
+    Expect<Equal<typeof numberSet, Set<number>>>,
+    Expect<Equal<typeof unknownSet, Set<unknown>>>,
+  ];
+}
+
+/**
+ * Passing default type arguments
+ */
+{
+  const createSet = <T = string>() => {
+    return new Set<T>();
+  };
+
+  const numberSet = createSet<number>();
+  const stringSet = createSet<string>();
+  const otherStringSet = createSet();
+
+  type Tests = [
+    Expect<Equal<typeof numberSet, Set<number>>>,
+    Expect<Equal<typeof stringSet, Set<string>>>,
+    Expect<Equal<typeof otherStringSet, Set<string>>>,
+  ];
+}
+
+/**
+ * Inferring types from type arguments
+ */
+{
+  class Component<TProps> {
+    private props: TProps;
+
+    constructor(props: TProps) {
+      this.props = props;
+    }
+
+    getProps = () => this.props;
+  }
+
+  const cloneComponent = <TProps>(component: Component<TProps>) => {
+    return new Component(component.getProps());
+  };
+
+  const component = new Component({ a: 1, b: 2, c: 3 });
+
+  const clonedComponent = cloneComponent(component);
+
+  const result = clonedComponent.getProps();
+
+  type Tests = [
+    Expect<Equal<typeof result, { a: number; b: number; c: number }>>,
+  ];
+}
