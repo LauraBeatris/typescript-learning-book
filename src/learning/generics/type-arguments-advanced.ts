fa0b2c139a849101103805af709ee6231b146603
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-10-25 18:54:39 -0300

    Add example for generic wrapper

--- src/learning/generics/type-arguments-advanced.ts
+++ src/learning/generics/type-arguments-advanced.ts
@@ -69,3 +69,118 @@ import { Equal, Expect } from '../../test-utils';
 
   type Tests = [Expect<Equal<typeof result1, Array<'a' | 'b'>>>];
 }
+
+/**
+ * Making a generic wrapper for a function
+ */
+{
+  const makeSafe1 =
+    <TParams extends any[], TReturn>(func: (...args: TParams) => TReturn) =>
+    (
+      ...args: TParams
+    ):
+      | {
+          type: 'success';
+          result: TReturn;
+        }
+      | {
+          type: 'failure';
+          error: Error;
+        } => {
+      try {
+        const result = func(...args);
+
+        return {
+          type: 'success',
+          result,
+        };
+      } catch (e) {
+        return {
+          type: 'failure',
+          error: e as Error,
+        };
+      }
+    };
+
+  const makeSafe2 =
+    <TFunc extends (...args: any[]) => any>(func: TFunc) =>
+    (
+      ...args: Parameters<TFunc>
+    ):
+      | {
+          type: 'success';
+          result: ReturnType<TFunc>;
+        }
+      | {
+          type: 'failure';
+          error: Error;
+        } => {
+      try {
+        const result = func(...args);
+
+        return {
+          type: 'success',
+          result,
+        };
+      } catch (e) {
+        return {
+          type: 'failure',
+          error: e as Error,
+        };
+      }
+    };
+
+  const func1 = makeSafe1(() => 1);
+
+  const func2 = makeSafe1(() => {
+    if (1 > 2) {
+      return '123';
+    }
+    throw new Error('Oh dear');
+  });
+
+  const func3 = makeSafe1((a: number, b: string) => {
+    return `${a} ${b}`;
+  });
+
+  const result1 = func1();
+
+  const result2 = func2();
+
+  // @ts-expect-error
+  func3();
+
+  // @ts-expect-error
+  func3(1, 1);
+
+  func3(1, '1');
+
+  type Tests = [
+    Expect<
+      Equal<
+        typeof result1,
+        | {
+            type: 'success';
+            result: number;
+          }
+        | {
+            type: 'failure';
+            error: Error;
+          }
+      >
+    >,
+    Expect<
+      Equal<
+        typeof result2,
+        | {
+            type: 'success';
+            result: string;
+          }
+        | {
+            type: 'failure';
+            error: Error;
+          }
+      >
+    >,
+  ];
+}
