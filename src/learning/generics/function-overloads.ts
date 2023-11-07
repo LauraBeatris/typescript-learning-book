2baac68f9b5cefba7e820de022abc1c251fd21d5
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-11-07 08:35:48 -0300

    Add example on matching return types with function overloads

--- src/learning/generics/function-overloads.ts
+++ src/learning/generics/function-overloads.ts
@@ -11,10 +11,36 @@ import { Equal, Expect } from "../../test-utils";
   function returnWhatIPassIn(t: unknown): unknown {
     return t;
   }
-  
+
   const one = returnWhatIPassIn(1);
   const matt = returnWhatIPassIn("matt");
-  
-  type Tests = [Expect<Equal<typeof one, 1>>, Expect<Equal<typeof matt, "matt">>];
-  
+
+  type Tests = [
+    Expect<Equal<typeof one, 1>>, 
+    Expect<Equal<typeof matt, "matt">>
+  ];
+}
+
+
+/**
+ * Match return types with function overloads
+ * 
+ * Tips: When doing a implementation signature, make sure to define a return type
+ * that matches the function signatures ones. This is due for function overloads
+ * not being that type safety.
+ */
+{
+  function youSayGoodbyeISayHello(greeting: "hello"): "goodbye";
+  function youSayGoodbyeISayHello(greeting: "goodbye"): "hello";
+  function youSayGoodbyeISayHello (greeting: "hello" | "goodbye"): "goodbye" | "hello" {
+    return greeting === "goodbye" ? "hello" : "goodbye";
+  };
+
+  const result1 = youSayGoodbyeISayHello("hello");
+  const result2 = youSayGoodbyeISayHello("goodbye");
+
+  type Tests = [
+    Expect<Equal<typeof result1, "goodbye">>, 
+    Expect<Equal<typeof result2, "hello">>
+  ];
 }
