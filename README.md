97bc99d49ebe8ae804fa53675130a34d33e92c4e
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-11-09 09:05:09 -0300

    Add example on when to use unions instead of overloads

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Throughout my TypeScript journey, modules will be added with usage examples acco
 Here you can refer to the modules structured by learning area, that can be used as examples for your learning journey as well.
 
 ### References
+
 - [Inference Basics](./src/learning/typescript-core/inference-basics.ts): Utilities and operators to define types based on the compiler inference.
 - [Template Literals](./src/learning/typescript-core/template-literals.ts): From TypeScript 4.1, Template Literals help to create object types, split strings into tuples and other useful transformations.
 - [Type Helpers](./src/learning/typescript-core/type-helpers.ts): Building type helpers on top of the ones within the language, as well setting constraints that ensure the flexibility and reusability of types.
--- src/learning/generics/function-overloads.ts
+++ src/learning/generics/function-overloads.ts
@@ -1,47 +1,48 @@
-import { Equal, Expect } from "../../test-utils";
+import { Equal, Expect } from '../../test-utils';
 
 /**
  * Understanding function overloads
- * 
+ *
  * Implementation signature vs function signature
  */
 {
   function returnWhatIPassIn(t: 1): 1;
-  function returnWhatIPassIn(t: "matt"): "matt";
+  function returnWhatIPassIn(t: 'matt'): 'matt';
   function returnWhatIPassIn(t: unknown): unknown {
     return t;
   }
 
   const one = returnWhatIPassIn(1);
-  const matt = returnWhatIPassIn("matt");
+  const matt = returnWhatIPassIn('matt');
 
   type Tests = [
-    Expect<Equal<typeof one, 1>>, 
-    Expect<Equal<typeof matt, "matt">>
+    Expect<Equal<typeof one, 1>>,
+    Expect<Equal<typeof matt, 'matt'>>,
   ];
 }
 
-
 /**
  * Match return types with function overloads
- * 
+ *
  * Tips: When doing a implementation signature, make sure to define a return type
  * that matches the function signatures ones. This is due for function overloads
  * not being that type safety.
  */
 {
-  function youSayGoodbyeISayHello(greeting: "hello"): "goodbye";
-  function youSayGoodbyeISayHello(greeting: "goodbye"): "hello";
-  function youSayGoodbyeISayHello (greeting: "hello" | "goodbye"): "goodbye" | "hello" {
-    return greeting === "goodbye" ? "hello" : "goodbye";
-  };
+  function youSayGoodbyeISayHello(greeting: 'hello'): 'goodbye';
+  function youSayGoodbyeISayHello(greeting: 'goodbye'): 'hello';
+  function youSayGoodbyeISayHello(
+    greeting: 'hello' | 'goodbye',
+  ): 'goodbye' | 'hello' {
+    return greeting === 'goodbye' ? 'hello' : 'goodbye';
+  }
 
-  const result1 = youSayGoodbyeISayHello("hello");
-  const result2 = youSayGoodbyeISayHello("goodbye");
+  const result1 = youSayGoodbyeISayHello('hello');
+  const result2 = youSayGoodbyeISayHello('goodbye');
 
   type Tests = [
-    Expect<Equal<typeof result1, "goodbye">>, 
-    Expect<Equal<typeof result2, "hello">>
+    Expect<Equal<typeof result1, 'goodbye'>>,
+    Expect<Equal<typeof result2, 'hello'>>,
   ];
 }
 
@@ -52,29 +53,29 @@ import { Equal, Expect } from "../../test-utils";
   interface AnonymousPrivileges {
     sitesCanVisit: string[];
   }
-  
+
   interface UserPrivileges extends AnonymousPrivileges {
     sitesCanEdit: string[];
   }
-  
+
   interface AdminPrivileges extends UserPrivileges {
     sitesCanDelete: string[];
   }
 
-  type Privileges = AnonymousPrivileges | UserPrivileges | AdminPrivileges
-  
-  function getRolePrivileges(role: "admin"): AdminPrivileges;
-  function getRolePrivileges(role: "user"): UserPrivileges;
+  type Privileges = AnonymousPrivileges | UserPrivileges | AdminPrivileges;
+
+  function getRolePrivileges(role: 'admin'): AdminPrivileges;
+  function getRolePrivileges(role: 'user'): UserPrivileges;
   function getRolePrivileges(role: string): AnonymousPrivileges;
   function getRolePrivileges(role: string): Privileges {
     switch (role) {
-      case "admin":
+      case 'admin':
         return {
           sitesCanDelete: [],
           sitesCanEdit: [],
           sitesCanVisit: [],
         };
-      case "user":
+      case 'user':
         return {
           sitesCanEdit: [],
           sitesCanVisit: [],
@@ -86,13 +87,77 @@ import { Equal, Expect } from "../../test-utils";
     }
   }
 
-  const adminPrivileges = getRolePrivileges("admin");
-  const userPrivileges = getRolePrivileges("user");
-  const anonymousPrivileges = getRolePrivileges("anonymous");
+  const adminPrivileges = getRolePrivileges('admin');
+  const userPrivileges = getRolePrivileges('user');
+  const anonymousPrivileges = getRolePrivileges('anonymous');
 
   type Tests = [
     Expect<Equal<typeof adminPrivileges, AdminPrivileges>>,
     Expect<Equal<typeof userPrivileges, UserPrivileges>>,
     Expect<Equal<typeof anonymousPrivileges, AnonymousPrivileges>>,
+  ];
+}
+
+/**
+ * When to use unions instead of overloads 
+ */
+{
+  // TODO: Apply typing for generator argument
+  // function runGenerator(generator: unknown) {
+  //   if (typeof generator === "function") {
+  //     return generator();
+  //   }
+  //   return generator.run();
+  // }
+
+  type Generator = {
+    run: () => string;
+  };
+
+  /**
+   * It's not worth to use function overloading here, considering that the
+   * return type doesn't change based on the argument
+   */
+  function runGenerator1<T extends Generator>(
+    generator: T,
+  ): ReturnType<T['run']>;
+  function runGenerator1<T extends Generator['run']>(
+    generator: T,
+  ): ReturnType<T>;
+  function runGenerator1(generator: Generator | Generator['run']): string {
+    if (typeof generator === 'function') {
+      return generator();
+    }
+    return generator.run();
+  }
+
+  /**
+   * Solution with union types, much cleaner to read. The result type is
+   * always the same no matter what's the combination of arguments
+   */
+  function runGenerator2(generator: Generator | Generator['run']): string {
+    if (typeof generator === 'function') {
+      return generator();
+    }
+    return generator.run();
+  }
+
+  const result1 = runGenerator1({
+    run: () => 'hello',
+  });
+
+  const result2 = runGenerator1(() => 'hello');
+
+  const result3 = runGenerator1({
+    run: () => 'hello',
+  });
+
+  const result4 = runGenerator1(() => 'hello');
+
+  type Tests = [
+    Expect<Equal<typeof result1, string>>,
+    Expect<Equal<typeof result2, string>>,
+    Expect<Equal<typeof result3, string>>,
+    Expect<Equal<typeof result4, string>>,
   ];
 }
