eea3fe3eb2a34b22f7b5144a804619df32e10fed
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-11-12 09:49:03 -0300

    Add example on splitting functions into two different call signatures

--- src/learning/generics/function-overloads.ts
+++ src/learning/generics/function-overloads.ts
@@ -211,3 +211,52 @@ import { Equal, Expect } from '../../test-utils';
     Expect<Equal<typeof divElement2, HTMLDivElement | null>>,
   ];
 }
+
+/**
+ * Split functions into two different call signatures
+ */
+{
+  function useData<T>(params: { fetchData: () => Promise<T> }): {
+    getData: () => T | undefined;
+  };
+  function useData<T>(params: {
+    fetchData: () => Promise<T>;
+    initialData: T;
+  }): {
+    getData: () => T;
+  };
+  function useData<T>(params: {
+    fetchData: () => Promise<T>;
+    initialData?: T;
+  }): {
+    getData: () => T | undefined;
+  } {
+    let data = params.initialData;
+
+    params.fetchData().then((d) => {
+      data = d;
+    });
+
+    return {
+      getData: () => data,
+    };
+  }
+
+  const numData1 = useData({
+    fetchData: () => Promise.resolve(1),
+  });
+
+  const data1 = numData1.getData();
+
+  const numData2 = useData({
+    fetchData: () => Promise.resolve(1),
+    initialData: 2,
+  });
+
+  const data2 = numData2.getData();
+
+  type Tests = [
+    Expect<Equal<typeof data1, number | undefined>>,
+    Expect<Equal<typeof data2, number>>,
+  ];
+}
