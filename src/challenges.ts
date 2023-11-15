2e65e65b78c268b03186e05523151ebfe84d06e4
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-11-15 09:58:58 -0300

    Add example on creating a function with a dynamic number of arguments

--- src/challenges.ts
+++ src/challenges.ts
@@ -233,3 +233,54 @@ import { S } from 'ts-toolbelt';
     Expect<Equal<typeof rows, Array<{ id: number; name: string }>>>,
   ];
 })();
+
+/**
+ * Create a function with a dynamic number of arguments
+ */
+{
+  interface Events {
+    click: {
+      x: number;
+      y: number;
+    };
+    focus: undefined;
+  }
+
+  const sendEvent = <TEventKey extends keyof Events>(
+    event: TEventKey,
+    ...args: Events[TEventKey] extends {} ? [payload: Events[TEventKey]] : []
+  ) => {
+    // Send the event somewhere!
+  };
+
+  // Should force you to pass a second argument when you choose an event with a payload
+
+  // @ts-expect-error
+  sendEvent('click');
+
+  sendEvent('click', {
+    // @ts-expect-error
+    x: 'oh dear',
+  });
+
+  sendEvent(
+    'click',
+    // @ts-expect-error
+    {
+      y: 1,
+    },
+  );
+
+  sendEvent('click', {
+    x: 1,
+    y: 2,
+  });
+
+  // Should prevent you from passing a second argument when you choose an event without a payload
+
+  sendEvent(
+    'focus',
+    // @ts-expect-error
+    {},
+  );
+}
