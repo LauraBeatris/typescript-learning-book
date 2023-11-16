9c8f06cf9ab5e2487ad8181ae66c023bd0861ff0
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-11-16 08:58:48 -0300

    Add example of extracting object properties with reduce and generics

--- src/challenges.ts
+++ src/challenges.ts
@@ -284,3 +284,44 @@ import { S } from 'ts-toolbelt';
     {},
   );
 }
+
+/**
+ * Extracting object properties with reduce and generics
+ */
+{
+  const pick = <TObj, TPicked extends keyof TObj>(
+    obj: TObj,
+    picked: Array<TPicked>,
+  ) => {
+    return picked.reduce((acc, key) => {
+      acc[key] = obj[key];
+      return acc;
+    }, {} as Pick<TObj, TPicked>);
+  };
+
+  const result = pick(
+    {
+      a: 1,
+      b: 2,
+      c: 3,
+    },
+    ['a', 'b'],
+  );
+
+  // Should not allow you to pass keys which do not exist in the object
+  pick(
+    {
+      a: 1,
+      b: 2,
+      c: 3,
+    },
+    [
+      'a',
+      'b',
+      // @ts-expect-error
+      'd',
+    ],
+  );
+
+  type Tests = Expect<Equal<typeof result, { a: number; b: number }>>;
+}
