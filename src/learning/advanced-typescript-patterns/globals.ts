0739f772205ac65f22cdf4e0a38adb1de7336d98
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-11-25 10:55:30 -0300

    Add example on using declaration merging to add functionally to the global window

--- src/learning/advanced-typescript-patterns/globals.ts
+++ src/learning/advanced-typescript-patterns/globals.ts
@@ -3,6 +3,10 @@ import { Equal, Expect } from '../../test-utils';
 declare global {
   function myFunc(): boolean;
   var myVar: number;
+
+  interface Window {
+    makeGreeting(): string;
+  }
 }
 
 /**
@@ -16,4 +20,17 @@ declare global {
     Expect<Equal<typeof myFunc, () => boolean>>,
     Expect<Equal<typeof myVar, number>>,
   ];
+}
+
+/**
+ * Use declaration merging to add functionally to the global window
+ *
+ * “declaration merging” means that the compiler merges two separate declarations
+ * declared with the same name into a single definition.
+ * @ref https://www.typescriptlang.org/docs/handbook/declaration-merging.html#handbook-content
+ */
+{
+  window.makeGreeting = () => 'Hello, world!';
+
+  type Tests = [Expect<Equal<typeof window.makeGreeting, () => string>>];
 }
