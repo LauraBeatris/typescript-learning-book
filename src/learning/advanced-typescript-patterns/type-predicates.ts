0af7a95c706a787854f7e1a0de5a35baa77cbc21
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-11-27 09:13:48 -0300

    Add example on ensuring valid types with assertion function functions

--- src/learning/advanced-typescript-patterns/type-predicates.ts
+++ src/learning/advanced-typescript-patterns/type-predicates.ts
@@ -12,3 +12,37 @@ import { Equal, Expect } from '../../test-utils';
 
   type Tests = [Expect<Equal<typeof filteredValues, string[]>>];
 }
+
+/**
+ * Ensuring valid types with an assertion function
+ */
+{
+  interface User {
+    id: string;
+    name: string;
+  }
+  
+  interface AdminUser extends User {
+    role: "admin";
+    organisations: string[];
+  }
+  
+  interface NormalUser extends User {
+    role: "normal";
+  }
+
+  /**
+   * @ref https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-7.html#assertion-functions
+   */
+  function assertUserIsAdmin(user: NormalUser | AdminUser): asserts user is AdminUser {
+    if (user.role !== "admin") {
+      throw new Error("Not an admin user");
+    }
+  }
+
+  const example = (user: NormalUser | AdminUser) => {
+    assertUserIsAdmin(user);
+
+    type Tests = [Expect<Equal<typeof user, AdminUser>>];
+  };
+}
