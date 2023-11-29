8ec5f88c6f85e2cf66cede54cbb8edc29d257f28
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-11-29 09:38:57 -0300

    Add example on using type predicates with generics

--- src/learning/advanced-typescript-patterns/type-predicates.ts
+++ src/learning/advanced-typescript-patterns/type-predicates.ts
@@ -21,22 +21,24 @@ import { Equal, Expect } from '../../test-utils';
     id: string;
     name: string;
   }
-  
+
   interface AdminUser extends User {
-    role: "admin";
+    role: 'admin';
     organisations: string[];
   }
-  
+
   interface NormalUser extends User {
-    role: "normal";
+    role: 'normal';
   }
 
   /**
    * @ref https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-7.html#assertion-functions
    */
-  function assertUserIsAdmin(user: NormalUser | AdminUser): asserts user is AdminUser {
-    if (user.role !== "admin") {
-      throw new Error("Not an admin user");
+  function assertUserIsAdmin(
+    user: NormalUser | AdminUser,
+  ): asserts user is AdminUser {
+    if (user.role !== 'admin') {
+      throw new Error('Not an admin user');
     }
   }
 
@@ -55,18 +57,18 @@ import { Equal, Expect } from '../../test-utils';
     id: string;
     name: string;
   }
-  
+
   interface AdminUser extends User {
-    role: "admin";
+    role: 'admin';
     organisations: string[];
   }
-  
+
   interface NormalUser extends User {
-    role: "normal";
+    role: 'normal';
   }
 
   /**
-   * Compiler error -> "Assertions require every name in the call target 
+   * Compiler error -> "Assertions require every name in the call target
    * to be declared with an explicit type annotation."
    */
   // const assertUserIsAdmin = (
@@ -83,14 +85,67 @@ import { Equal, Expect } from '../../test-utils';
   function assertUserIsAdmin(
     user: NormalUser | AdminUser,
   ): asserts user is AdminUser {
-    if (user.role !== "admin") {
-      throw new Error("Not an admin user");
+    if (user.role !== 'admin') {
+      throw new Error('Not an admin user');
     }
-  };
+  }
 
   const example = (user: NormalUser | AdminUser) => {
     assertUserIsAdmin(user);
 
     type tests = [Expect<Equal<typeof user, AdminUser>>];
   };
+}
+
+/**
+ * Filtering with type predicates and generics
+ */
+{
+  const isDivElement = (element: unknown): element is HTMLDivElement => {
+    return element instanceof HTMLDivElement;
+  };
+
+  const isAnchorElement = (element: unknown): element is HTMLAnchorElement => {
+    return element instanceof HTMLAnchorElement;
+  };
+
+  const isBodyElement = (element: unknown): element is HTMLBodyElement => {
+    return element instanceof HTMLBodyElement;
+  };
+
+  interface DOMNodeExtractorConfig<T, Result> {
+    isNode: (node: unknown) => node is T;
+    transform: (node: T) => Result;
+  }
+
+  const createDOMNodeExtractor = <T, TResult>(
+    config: DOMNodeExtractorConfig<T, TResult>,
+  ) => {
+    return (nodes: T[]): TResult[] => {
+      return nodes.filter(config.isNode).map(config.transform);
+    };
+  };
+
+  // Should pick up that `extractDivs` is of type `HTMLDivElement[]`
+  const extractDivs = createDOMNodeExtractor({
+    isNode: isDivElement,
+
+    transform: (div) => {
+      type test1 = Expect<Equal<typeof div, HTMLDivElement>>;
+      return div.innerText;
+    },
+  });
+
+  const extractAnchors = createDOMNodeExtractor({
+    isNode: isAnchorElement,
+
+    transform: (a) => {
+      type test1 = Expect<Equal<typeof a, HTMLAnchorElement>>;
+      return a.innerText;
+    },
+  });
+
+  const divs = extractDivs([document.createElement('div')]);
+
+  type test2 = Expect<Equal<typeof divs, string[]>>;
 }
