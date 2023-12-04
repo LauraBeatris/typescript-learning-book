bf944b0edd87849295d2e0ae702174988789b6a3
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-04 09:15:28 -0300

    Add example on getters and setters in the builder pattern

--- src/learning/advanced-typescript-patterns/classes-builder-pattern.ts
+++ src/learning/advanced-typescript-patterns/classes-builder-pattern.ts
@@ -127,3 +127,40 @@ import { Equal, Expect } from '../../test-utils';
     }
   }
 }
+
+/**
+ * Getters and setters in the builder pattern
+ */
+{
+  class TypeSafeStringMap<TMap extends Record<string, string> = {}> {
+    private map: TMap;
+
+    constructor() {
+      this.map = {} as TMap;
+    }
+
+    get(key: keyof TMap): string {
+      return this.map[key];
+    }
+
+    set<K extends string>(
+      key: K,
+      value: string,
+    ): TypeSafeStringMap<TMap & Record<K, string>> {
+      (this.map[key] as any) = value;
+
+      return this;
+    }
+  }
+
+  const map = new TypeSafeStringMap()
+    .set('matt', 'pocock')
+    .set('jools', 'holland')
+    .set('brandi', 'carlile');
+
+  // Should not allow getting values which do not exist
+  map.get(
+    // @ts-expect-error
+    'jim',
+  );
+}
