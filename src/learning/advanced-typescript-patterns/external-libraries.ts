da43234f157fdbae3f32c1abb9e444dd37d03845
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-08 08:56:05 -0300

    Add example on passing type arguments with Lodash

--- src/learning/advanced-typescript-patterns/external-libraries.ts
+++ src/learning/advanced-typescript-patterns/external-libraries.ts
@@ -1,3 +1,4 @@
+import _, { List } from 'lodash';
 import { fetchUser } from '../../helpers';
 import { Equal, Expect, ExpectExtends } from '../../test-utils';
 
@@ -31,5 +32,46 @@ import { Equal, Expect, ExpectExtends } from '../../test-utils';
         ReturnTypeOfFetchUserWithFullName
       >
     >,
+  ];
+}
+
+/**
+ * Passing type arguments with Lodash
+ */
+{
+  const groupByAge = <T extends { age: number }>(array: List<T>) => {
+    const grouped = _.groupBy<T>(array, 'age');
+
+    return grouped;
+  };
+
+  const result = groupByAge([
+    {
+      name: 'John',
+      age: 20,
+    },
+    {
+      name: 'Jane',
+      age: 20,
+    },
+    {
+      name: 'Mary',
+      age: 30,
+    },
+  ]);
+
+  groupByAge([
+    {
+      // @ts-expect-error
+      name: 'John',
+    },
+    {
+      // @ts-expect-error
+      name: 'Bill',
+    },
+  ]);
+
+  type Tests = [
+    Expect<Equal<typeof result, _.Dictionary<{ name: string; age: number }[]>>>,
   ];
 }
