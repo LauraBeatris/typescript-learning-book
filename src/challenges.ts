de03fd749a11098a1f6e0d85e946727323c74ca6
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-16 12:55:08 -0300

    Add challenge of creating a type-safe request handler with Zod and Express

--- src/challenges.ts
+++ src/challenges.ts
@@ -1,4 +1,7 @@
 import { S } from 'ts-toolbelt';
+import { z, ZodError } from 'zod';
+import express, { Request, RequestHandler } from 'express';
+import { ParsedQs } from 'qs';
 import { Equal, Expect } from './support/test-utils';
 
 /**
@@ -579,4 +582,73 @@ declare global {
       Equal<typeof banana, { readonly name: 'banana'; readonly price: 2 }>
     >,
   ];
+}
+
+/**
+ * Creating a type-safe request handler with Zod and Express
+ */
+{
+  const makeTypeSafeHandler = <
+    TQuery extends ParsedQs = any,
+    TBody extends Record<string, any> = any,
+  >(
+    config: {
+      query?: z.Schema<TQuery>;
+      body?: z.Schema<TBody>;
+    },
+    handler: RequestHandler<any, any, TBody, TQuery>,
+  ): RequestHandler<any, any, TBody, TQuery> => {
+    return (req: Request<any, any, TBody, TQuery>, res, next) => {
+      const { query, body } = req;
+      if (config.query) {
+        try {
+          config.query.parse(query);
+        } catch (e) {
+          return res.status(400).send((e as ZodError).message);
+        }
+      }
+      if (config.body) {
+        try {
+          config.body.parse(body);
+        } catch (e) {
+          return res.status(400).send((e as ZodError).message);
+        }
+      }
+      return handler(req, res, next);
+    };
+  };
+
+  const app = express();
+
+  // Should make the query AND body type safe
+  app.get(
+    '/users',
+    makeTypeSafeHandler(
+      {
+        query: z.object({
+          id: z.string(),
+        }),
+        body: z.object({
+          name: z.string(),
+        }),
+      },
+      (req, res) => {
+        type tests = [
+          Expect<Equal<typeof req.query, { id: string }>>,
+          Expect<Equal<typeof req.body, { name: string }>>,
+        ];
+      },
+    ),
+  );
+
+  // Should default them to any if not passed in config
+  app.get(
+    '/users',
+    makeTypeSafeHandler({}, (req, res) => {
+      type tests = [
+        Expect<Equal<typeof req.query, any>>,
+        Expect<Equal<typeof req.body, any>>,
+      ];
+    }),
+  );
 }
