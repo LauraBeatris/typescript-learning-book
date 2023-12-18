fa20d99e87d087e545e55a3e00fe0311ca8739ec
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-18 09:41:43 -0300

    Add example on differentiating props with a boolean discriminator

--- src/learning/react-with-typescript/advanced-props.tsx
+++ src/learning/react-with-typescript/advanced-props.tsx
@@ -176,3 +176,52 @@
     );
   };
 }
+
+/**
+ * Differentiating props with a boolean discriminator
+ */
+{
+  // type EmbeddedPlaygroundProps = {
+  //   useStackblitz?: boolean;
+  //   stackblitzId?: string;
+  //   codeSandboxId?: string;
+  // };
+
+  type EmbeddedPlaygroundProps =
+    | {
+        useStackblitz: true;
+        stackblitzId: string;
+      }
+    | {
+        useStackblitz?: false;
+        codeSandboxId: string;
+      };
+
+  const EmbeddedPlayground = (props: EmbeddedPlaygroundProps) => {
+    if (props.useStackblitz) {
+      return (
+        <iframe
+          src={`https://stackblitz.com/edit/${props.stackblitzId}?embed=1`}
+        />
+      );
+    }
+
+    return (
+      <iframe src={`https://codesandbox.io/embed/${props.codeSandboxId}`} />
+    );
+  };
+
+  <>
+    <EmbeddedPlayground useStackblitz stackblitzId="my-stackblitz-id" />
+    <EmbeddedPlayground codeSandboxId="my-codesandbox-id" />
+
+    <EmbeddedPlayground
+      useStackblitz
+      // @ts-expect-error
+      codeSandboxId="my-codesandbox-id"
+    />
+
+    {/* @ts-expect-error */}
+    <EmbeddedPlayground stackblitzId="my-stackblitz-id" />
+  </>;
+}
