1744f5813d38b04be9d3ec286b5fee5ba8c67f4d
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-24 12:43:20 -0300

    Add example on passing type arguments to components

--- src/learning/react-with-typescript/generics-with-components.tsx
+++ src/learning/react-with-typescript/generics-with-components.tsx
@@ -283,3 +283,61 @@ import { Equal, Expect } from '../../support/test-utils';
     );
   };
 }
+
+/**
+ * Passing type arguments to components
+ */
+{
+  interface TableProps<TRow> {
+    rows: TRow[];
+    renderRow: (row: TRow) => React.ReactNode;
+  }
+
+  function Table<TRow>(props: TableProps<TRow>) {
+    return (
+      <table>
+        <tbody>
+          {props.rows.map((row) => (
+            <tr>{props.renderRow(row)}</tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+
+  interface User {
+    id: number;
+    name: string;
+    age: number;
+  }
+
+  <>
+    <Table<User>
+      // @ts-expect-error rows should be User[]
+      rows={[1, 2, 3]}
+      renderRow={(row) => {
+        type test = Expect<Equal<typeof row, User>>;
+        return <td>{row.name}</td>;
+      }}
+    />
+    <Table<User>
+      rows={[
+        {
+          id: 1,
+          name: 'John',
+          age: 30,
+        },
+        {
+          // @ts-expect-error id should be string
+          id: '2',
+          name: 'Jane',
+          age: 30,
+        },
+      ]}
+      renderRow={(row) => {
+        type test = Expect<Equal<typeof row, User>>;
+        return <td>{row.name}</td>;
+      }}
+    ></Table>
+  </>;
+}
