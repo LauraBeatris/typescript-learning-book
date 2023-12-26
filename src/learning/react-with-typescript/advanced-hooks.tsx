288486997a46203da375f0e54ab2cc25510a7292
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-26 10:44:14 -0300

    Add example on using unions to manage states

--- src/learning/react-with-typescript/advanced-hooks.tsx
+++ src/learning/react-with-typescript/advanced-hooks.tsx
@@ -1,3 +1,4 @@
+import { appendVideoToDomAndPlay, fetchVideo } from '../../support/helpers';
 import { Equal, Expect } from '../../support/test-utils';
 
 /**
@@ -83,8 +84,67 @@ import { Equal, Expect } from '../../support/test-utils';
           <ThemeProvider value={{ primaryColor: 'blue' }}>
             <Child />
           </ThemeProvider>
- </UserProvider>
+        </UserProvider>
       </>
     );
+  };
+}
+
+/**
+ * Using TypeScript to manage complex state
+ *
+ * Solution: Handling complex state management with unions
+ */
+{
+  type State = 'loading' | 'loaded' | 'error';
+
+  const useLoadAsyncVideo = (src: string) => {
+    const [state, setState] = React.useState<State>('loading');
+
+    React.useEffect(() => {
+      setState('loading');
+
+      let cancelled = false;
+
+      fetchVideo(src)
+        .then((blob) => {
+          if (cancelled) {
+            return;
+          }
+
+          appendVideoToDomAndPlay(blob);
+
+          setState('loaded');
+        })
+        .catch((error) => {
+          if (cancelled) {
+            return;
+          }
+          setState('error');
+        });
+
+      return () => {
+        cancelled = true;
+      };
+    }, [src]);
+
+    // @ts-expect-error
+    if (state === 'does-not-exist') {
+    }
+
+    if (state === 'loading') {
+      return 'loading...';
+    }
+
+    if (state === 'loaded') {
+      return 'loaded';
+    }
+
+    if (state === 'error') {
+      return 'Error!';
+    }
+
+    // state should equal never! Because we've covered all the cases
+    type Tests = Expect<Equal<typeof state, never>>;
   };
 }
