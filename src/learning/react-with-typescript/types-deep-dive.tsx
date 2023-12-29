074211b7cd3b9e257e288f488ecf4c87d086fc6d
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-29 19:28:00 -0300

    Add example on the usage of the React namespace

---
+++ src/learning/react-with-typescript/types-deep-dive.tsx
@@ -0,0 +1,14 @@
+/**
+ * Exploring the React namespace
+ */
+{
+  /**
+   * Questions to answer 👇
+   */
+
+  // 1. What is the React namespace?
+  type Example = React.ReactNode;
+
+  // 2. How come React can be used BOTH as a value and a type?
+  const element = React.createElement('div');
+}
