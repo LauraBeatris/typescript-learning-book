614e371923ce18c68e08c4fcc2e4447eaf97ff17
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-30 13:31:31 -0300

    Add example on understanding the structure of `JSX.IntrinsicElements`

--- src/learning/react-with-typescript/types-deep-dive.tsx
+++ src/learning/react-with-typescript/types-deep-dive.tsx
@@ -94,3 +94,35 @@
     <Option />
   </Select>;
 }
+
+/**
+ * Understanding the structure of React's JSX.IntrinsicElements
+ */
+{
+  /**
+   * 1. What is JSX.IntrinsicElements? CMD-click on .IntrinsicElements below
+   * to go to its definition.
+   */
+  type Example = JSX.IntrinsicElements;
+
+  /**
+   * 2. What is the structure of JSX.IntrinsicElements? It appears to have the
+   * HTML attributes as properties, but what are the values?
+   *
+   * 3. Let's have some fun. Edit the file to add a new property to
+   * JSX.IntrinsicElements:
+   *
+   * interface IntrinsicElements {
+   *   // ...
+   *   myNewElement: {
+   *     foo: string;
+   *   }
+   * }
+   *
+   * Notice that the error below goes away!
+   *
+   * 4. Now change it back, before anyone notices.
+   */
+
+  // <myNewElement foo="123" />;
+}
