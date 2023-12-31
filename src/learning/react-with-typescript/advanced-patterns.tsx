7913524135dcf0860ed4ae80e1b7b0ee1edfe4a8
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-31 13:27:44 -0300

    Add example of the render props pattern

--- src/learning/react-with-typescript/advanced-patterns.tsx
+++ src/learning/react-with-typescript/advanced-patterns.tsx
@@ -1,3 +1,6 @@
+import { createPortal } from 'react-dom';
+import { Equal, Expect } from '../../support/test-utils';
+
 /**
  * Strongly typing lazy loaded components with generics
  */
@@ -37,4 +40,104 @@
       loader={() => import('../../support/fake-external-library-component')}
     />
   </>;
+}
+
+/**
+ * Render props pattern: Typing the children prop for render props
+ */
+{
+  interface ModalChildProps {
+    isOpen: boolean;
+    openModal: () => void;
+    closeModal: () => void;
+  }
+
+  /**
+   * First approach: Using `children` property
+   */
+  const Modal1 = ({
+    children,
+  }: {
+    // children: (props: ModalChildProps) => React.ReactNode;
+    children: React.FC<ModalChildProps>;
+  }) => {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    return (
+      <>
+        {children({
+          isOpen,
+          openModal: () => setIsOpen(true),
+          closeModal: () => setIsOpen(false),
+        })}
+        {createPortal(
+          <div>
+            <h1>Modal</h1>
+          </div>,
+          document.getElementById('modal-root')!,
+        )}
+      </>
+    );
+  };
+
+  /**
+   * Second approach: Passing a `render` property
+   */
+  const Modal2 = ({
+    renderButtons,
+  }: {
+    renderButtons: React.FC<ModalChildProps>;
+  }) => {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    return (
+      <>
+        {renderButtons({
+          isOpen,
+          openModal: () => setIsOpen(true),
+          closeModal: () => setIsOpen(false),
+        })}
+        {createPortal(
+          <div>
+            <h1>Modal</h1>
+          </div>,
+          document.getElementById('modal-root')!,
+        )}
+      </>
+    );
+  };
+
+  const Parent1 = () => {
+    return (
+      <Modal1>
+        {(props) => {
+          type Tests = Expect<Equal<typeof props, ModalChildProps>>;
+
+          return (
+            <>
+              <button onClick={props.openModal}>Open Modal</button>
+              <button onClick={props.closeModal}>Close Modal</button>
+            </>
+          );
+        }}
+      </Modal1>
+    );
+  };
+
+  const Parent2 = () => {
+    return (
+      <Modal2
+        renderButtons={(props) => {
+          type test = Expect<Equal<typeof props, ModalChildProps>>;
+
+          return (
+            <>
+              <button onClick={props.openModal}>Open Modal</button>
+              <button onClick={props.closeModal}>Close Modal</button>
+            </>
+          );
+        }}
+      />
+    );
+  };
 }
